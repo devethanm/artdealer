ae891bd6deaad4047a485f6f957757b0598ef32b
devethanm <ethanmdev@gmail.com>
2022-10-08 01:53:21 -0700

    added introduction text and start of navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <div className="flex top-0 bg-black text-white pb-2 pt-2">
+            <i>A</i>    
+            <i>B</i>    
+            <i>C</i>    
+            <i>D</i>    
+        </div>
+    )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Navbar from "../components/navbar";
 
 const Home: NextPage = () => {
   return (
-    <div className="h-screen bg-slate-500">
+    <div className="h-screen bg-slate-900">
       <Head>
         <title>artdealer</title>
       </Head>
-      <body>
-        <h1 className="text-center text-2xl font-bold text-teal-400">Hello World</h1>
-      </body>
+      <Navbar></Navbar>
+      <div className="justify-center flex">
+        <h1 className="inline text-2xl font-bold text-green-700 mr-2">
+          Welcome to
+        </h1>
+        <h1 className="text-center text-2xl font-bold text-red-400  inline">
+          art
+        </h1>
+        <h1 className="text-center text-2xl font-bold text-slate-400 inline">
+          dealer
+        </h1>
+      </div>
     </div>
   );
 };
