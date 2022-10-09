227d2660e418f1431134abce41af2a02e43291ac
devethanm <ethanmdev@gmail.com>
2022-10-08 22:35:18 -0700

    added search bar section

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,27 @@ const Home: NextPage = () => {
       <Head>
         <title>artdealer</title>
       </Head>
-      { /*Place a navigation bar at the top of the screen*/ }
+      {/*Place a navigation bar at the top of the screen*/}
       <Navbar></Navbar>
+
+      { /* Welcome text */ }
       <div className="justify-center text-center flex">
-        <h1 className="text-2xl font-bold text-green-700">
-          welcome to&nbsp;
-        </h1>
-        <h1 className="text-center text-2xl font-bold text-red-400 ">
-          art
-        </h1>
+        <h1 className="text-2xl font-bold text-green-700">welcome to&nbsp;</h1>
+        <h1 className="text-center text-2xl font-bold text-red-400 ">art</h1>
         <h1 className="text-center text-2xl font-bold text-slate-400 ">
           dealer
         </h1>
       </div>
+
+      { /* Searchbar Section */ }
+      <div className="justify-center text-white text-center">
+        <p>Please enter search term:</p>
+        <input className="text-black text-center mb-2"></input>
+        <div>
+          <button className="text-black border-r-slate-600 bg-slate-200">Search</button>
+        </div>
+      </div>
+
     </div>
   );
 };
